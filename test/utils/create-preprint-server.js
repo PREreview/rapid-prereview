1d5335be9662d84a56349b13940379476d60aaa0
sballesteros <sebastien.ballesteros@gmail.com>
2019-10-06 19:25:59 +0200

    add preprint server

--- test/utils/create-preprint-server.js
+++ test/utils/create-preprint-server.js
@@ -2,8 +2,12 @@ import http from 'http';
 import path from 'path';
 import fs from 'fs';
 import express from 'express';
+import { promisify } from 'util';
+import fetch from 'node-fetch';
 import { unprefix } from '../../src/utils/jsonld';
 import { rapid } from '../../src/';
+
+const writeFile = promisify(fs.writeFile);
 
 const filenames = fs
   .readdirSync(path.resolve(__dirname, '../fixtures'))
@@ -57,4 +61,55 @@ export function createConfig(port) {
     baseUrlCrossref: `http://127.0.0.1:${port}/crossref/`,
     baseUrlOpenAire: `http://127.0.0.1:${port}/openaire/`
   };
+}
+
+/**
+ * Used to populate the test/fixtures directory
+ */
+export async function harvest(
+  identifier // doi:10.1101/791038 or arXiv:1910.00274
+) {
+  const root = path.resolve(__dirname, '../fixtures');
+
+  if (identifier.startsWith('arXiv:')) {
+    const r = await fetch(
+      `http://export.arxiv.org/oai2?verb=GetRecord&metadataPrefix=oai_dc&identifier=oai:arXiv.org:${unprefix(
+        identifier
+      )}`
+    );
+    const text = await r.text();
+    await writeFile(
+      path.join(root, `arxiv-${unprefix(identifier).replace(/\./g, '-')}.xml`),
+      text,
+      'utf8'
+    );
+  } else if (identifier.includes('zenodo')) {
+    const r = await fetch(
+      `http://api.openaire.eu/search/publications?doi=${unprefix(identifier)}`
+    );
+    const text = await r.text();
+    await writeFile(
+      path.join(
+        root,
+        `openaire-${unprefix(identifier).replace(/\./g, '-')}.xml`
+      ),
+      text,
+      'utf8'
+    );
+  } else {
+    const r = await fetch(
+      `https://api.crossref.org/works/${unprefix(identifier)}`
+    );
+    const text = await r.text();
+    await writeFile(
+      path.join(
+        root,
+        `crossref-${unprefix(identifier)
+          .replace(/\./g, '-')
+          .replace(/\//g, '_')}.json`
+      ),
+      text,
+      'utf8'
+    );
+  }
 }
