cf306221ef7dae812733f2953a49d1ef645f7ccb
halmos <ewysocan@gmail.com>
2019-10-07 14:20:51 -0400

    adds css to storybook config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.css$/,
+    sideEffects: true,
+
+    use: [
+      {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          sourceMap: true
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: function(loader) {
+            return [
+              require('postcss-import')(),
+              require('postcss-url')(),
+              require('postcss-preset-env')({
+                /* see: https://github.com/csstools/postcss-preset-env/issues/32 */
+                browsers: 'last 2 versions',
+                stage: 3,
+                features: {
+                  'nesting-rules': false /* disable css nesting which does not allow nesting of selectors without white spaces between them */,
+                  'custom-media-queries': true
+                }
+              }),
+              require('postcss-nested') /*replace cssnext nesting with this one which allows for sass style nesting*/
+            ];
+          }
+        }
+      }
+    ],
+    include: [path.resolve(__dirname, 'src')] // does this work with imports?
+  });
+  return config;
+};
---
+++ src/components/home.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './home';
+
+export default { title: 'home' };
+
+export function empty() {
+  return <Home />;
+}
