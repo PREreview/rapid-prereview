103ced9338cebac4836b75d60887f4cd069560da
sballesteros <sebastien.ballesteros@gmail.com>
2019-10-11 19:10:46 +0200

    barplot markup

---
+++ src/components/barplot.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export default function Barplot() {
+  return (
+    <div className="barplot">
+      <ul>
+        <li>
+          <table>
+            <caption>Result for Q1:</caption>
+            <thead>
+              <tr>
+                <th>yes</th>
+                <th>no</th>
+                <th>unsure</th>
+                <th>n.a.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>10%</td>
+                <td>20%</td>
+                <td>30%</td>
+                <td>40%</td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+
+        <li>
+          <table>
+            <caption>Result for Q2:</caption>
+            <thead>
+              <tr>
+                <th>yes</th>
+                <th>no</th>
+                <th>unsure</th>
+                <th>n.a.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>10%</td>
+                <td>20%</td>
+                <td>30%</td>
+                <td>40%</td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+      </ul>
+    </div>
+  );
+}
