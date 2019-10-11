292035c54b073a238aa9f7637e8038e8c55b9acc
halmos <ewysocan@gmail.com>
2019-10-11 10:35:14 -0400

    improves app layout

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,12 +4,14 @@ import { MdSearch } from 'react-icons/md';
 export default function SearchBar() {
   return (
     <div className="search-bar">
+      <div className="search-bar__left-spacer" />
       <div className="search-bar__search-box">
         <input type="text" className="search-bar__search-box__input" />
         <button className="search-bar__search-box__button">
           <MdSearch className="search-bar__search-box__button-icon" />
         </button>
       </div>
+      <div className="search-bar__right-spacer" />
     </div>
   );
 }
