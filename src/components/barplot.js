6685858221803b0cf9295212bbbd85b4ebb53351
halmos <ewysocan@gmail.com>
2019-10-11 15:44:07 -0400

    styles the barplot

--- src/components/barplot.js
+++ src/components/barplot.js
@@ -3,46 +3,102 @@ import React from 'react';
 export default function Barplot() {
   return (
     <div className="barplot">
-      <ul>
-        <li>
-          <table>
-            <caption>Result for Q1:</caption>
-            <thead>
-              <tr>
-                <th>yes</th>
-                <th>no</th>
-                <th>unsure</th>
-                <th>n.a.</th>
+      <ul className="barplot__question-list">
+        <li className="barplot__question-list__item">
+          <table className="barplot__question-table" tabIndex={0}>
+            <caption className="barplot__question">Result for Q1:</caption>
+            <thead className="barplot__table-header">
+              <tr className="barplot__segment-titles">
+                <th className="barplot__segment-title" style={{ width: '10%' }}>
+                  yes
+                </th>
+                <th className="barplot__segment-title" style={{ width: '20%' }}>
+                  no
+                </th>
+                <th className="barplot__segment-title" style={{ width: '30%' }}>
+                  unsure
+                </th>
+                <th className="barplot__segment-title" style={{ width: '40%' }}>
+                  n.a.
+                </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>10%</td>
-                <td>20%</td>
-                <td>30%</td>
-                <td>40%</td>
+            <tbody className="barplot__responses">
+              <tr className="barplot__bar-segments">
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--yes"
+                  style={{ width: '10%' }}
+                >
+                  10%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--no"
+                  style={{ width: '20%' }}
+                >
+                  20%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--unsure"
+                  style={{ width: '30%' }}
+                >
+                  30%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--na"
+                  style={{ width: '40%' }}
+                >
+                  40%
+                </td>
               </tr>
             </tbody>
           </table>
         </li>
 
-        <li>
-          <table>
-            <caption>Result for Q2:</caption>
-            <thead>
-              <tr>
-                <th>yes</th>
-                <th>no</th>
-                <th>unsure</th>
-                <th>n.a.</th>
+        <li className="barplot__question-list__item">
+          <table className="barplot__question-table" tabIndex={0}>
+            <caption className="barplot__question">Result for Q2:</caption>
+            <thead className="barplot__table-header">
+              <tr className="barplot__segment-titles">
+                <th className="barplot__segment-title" style={{ width: '10%' }}>
+                  yes
+                </th>
+                <th className="barplot__segment-title" style={{ width: '20%' }}>
+                  no
+                </th>
+                <th className="barplot__segment-title" style={{ width: '30%' }}>
+                  unsure
+                </th>
+                <th className="barplot__segment-title" style={{ width: '40%' }}>
+                  n.a.
+                </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>10%</td>
-                <td>20%</td>
-                <td>30%</td>
-                <td>40%</td>
+            <tbody className="barplot__responses">
+              <tr className="barplot__bar-segments">
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--yes"
+                  style={{ width: '10%' }}
+                >
+                  10%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--no"
+                  style={{ width: '20%' }}
+                >
+                  20%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--unsure"
+                  style={{ width: '30%' }}
+                >
+                  30%
+                </td>
+                <td
+                  className="barplot__bar-segment barplot__bar-segment--na"
+                  style={{ width: '40%' }}
+                >
+                  40%
+                </td>
               </tr>
             </tbody>
           </table>
