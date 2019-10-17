0e14d73e20173c5fbcacc59b4638f780a7b09b39
sballesteros <sebastien.ballesteros@gmail.com>
2019-10-17 21:10:22 +0200

    role activity

--- src/components/role-activity.js
+++ src/components/role-activity.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+import { getId } from '../utils/jsonld';
 import { useActionsSearchResults } from '../hooks/api-hooks';
+import Value from './value';
 
 export default function RoleActivity({ roleId }) {
   const [query, setQuery] = useState(
@@ -12,9 +16,29 @@ export default function RoleActivity({ roleId }) {
   const [results, progress] = useActionsSearchResults(query);
 
   return (
-    <div>
-      hello activity
-      <pre>{JSON.stringify(results, null, 2)}</pre>
+    <div className="role-activity">
+      {!!(results.counts && results.counts['@type']) && (
+        <dl>
+          <dt>Number of requests</dt>
+          <dd>{results.counts['@type']['RequestForRapidPREreviewAction']}</dd>
+          <dt>Number of reviews</dt>
+          <dd>{results.counts['@type']['RapidPREreviewAction']}</dd>
+        </dl>
+      )}
+
+      <ul>
+        {results.rows.map(({ doc }) => (
+          <li key={getId(doc)}>
+            {format(new Date(doc.startTime), 'MMM. d, yyyy')}{' '}
+            {doc['@type'] === 'RequestForRapidPREreviewAction'
+              ? 'requested feedback on'
+              : 'reviewed'}{' '}
+            <Link to={`/${doc.object.doi || doc.object.arXivId}`}>
+              <Value tagName="span">{doc.object.name}</Value>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
