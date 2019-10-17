462beffff3b470f441b521e892037c370ed88e42
sballesteros <sebastien.ballesteros@gmail.com>
2019-10-17 17:51:03 +0200

    profile header

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { format } from 'date-fns';
 import HeaderBar from './header-bar';
 import { useRole } from '../hooks/api-hooks';
 import RoleActivity from './role-activity';
@@ -14,13 +15,46 @@ export default function Profile() {
 
   const [role, fetchRoleProgress] = useRole(roleId);
 
+  const user = role && role.isRoleOf;
+
   return (
     <div className="profile">
       <HeaderBar />
 
-      <h2>Profile</h2>
+      <header>
+        {role && role.avatar && role.avatar.contentUrl ? (
+          <img src={role.avatar.contentUrl} alt="avatar" />
+        ) : null}
+        <h2>{role && role.name ? role.name : unprefixedRoleId}</h2>
 
-      {!!role && <pre>{JSON.stringify(role, null, 2)}</pre>}
+        <dl>
+          <dt>Rapid PREreview identifier</dt>
+          <dd>
+            <Link to={`/about/${unprefixedRoleId}`}>{unprefixedRoleId}</Link>
+          </dd>
+          {!!role && (
+            <Fragment>
+              <dt>Identity</dt>
+              <dd>
+                {role['@type'] === 'AnonymousReviewerRole'
+                  ? 'Anonymous'
+                  : 'Public'}
+              </dd>
+            </Fragment>
+          )}
+
+          {!!user && (
+            <Fragment>
+              <dt>ORCID</dt>
+              <dd>
+                <a href={`https://orcid.org/${user.orcid}`}>{user.orcid}</a>
+              </dd>
+              <dt>Member since</dt>
+              <dd>{format(new Date(user.dateCreated), 'MMM. d, yyyy')}</dd>
+            </Fragment>
+          )}
+        </dl>
+      </header>
 
       <h2>Activity</h2>
 
