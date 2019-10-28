00050bbf984e6c4b922ac821ca302e208f516f72
sballesteros <sebastien.ballesteros@gmail.com>
2019-10-28 10:51:33 -0700

    basic extension wiring

--- src/content-script.js
+++ src/content-script.js
@@ -1,1 +1,34 @@
-// TODO
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/popup';
+import './content-script.css';
+
+if (
+  document.readyState === 'interactive' ||
+  document.readyState === 'complete'
+) {
+  start();
+} else {
+  document.addEventListener('DOMContentLoaded', start);
+}
+
+function start() {
+  const $div = document.createElement('div');
+  document.body.appendChild($div);
+
+  function detect() {
+    const hasGscholar = !!document.head.querySelector(
+      'meta[name^="citation_"], meta[property^="citation_"]'
+    );
+
+    if (hasGscholar) {
+      ReactDOM.render(<Popup />, $div);
+    }
+  }
+
+  window.onpopstate = function(event) {
+    detect();
+  };
+
+  detect();
+}
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,8 @@
-// TODO
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/popup';
+import './popup.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Popup />, document.getElementById('app'));
+});
