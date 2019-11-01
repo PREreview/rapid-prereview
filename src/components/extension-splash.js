219b0d3ef54bcc927c08b514339331bee5d6a222
halmos <ewysocan@gmail.com>
2019-11-01 17:49:40 -0400

    styles app page

--- src/components/extension-splash.js
+++ src/components/extension-splash.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import HeaderBar from './header-bar';
+import Button from './button';
+import LabelStyle from './label-style';
 
 export default function ExtensionSplash() {
   return (
     <div className="extension-splash">
       <HeaderBar />
+      <div className="extension-splash__content">
+        <h1 className="extension-splash__title">
+          Add Rapid PREreview to your browser
+        </h1>
 
-      <h1>Add Rapid PREreview to your browser</h1>
+        <div className="extension-splash__body">
+          <p>
+            The Outbreak Science Rapid PREreview app lets you read and add
+            reviews (or requests for feedback) directly from the preprint sites
+            you visit without having to navigate to the Rapid PREreview
+            homepage.
+          </p>
+          <img
+            src="images/extension-preview.png"
+            className="extension-splash__screenshot"
+          />
+          <ul className="extension-splash__browser-list">
+            <li className="extension-splash__browser-list-item">
+              <Button
+                className="extension-splash__install-button"
+                primary={true}
+              >
+                Install for Chrome
+              </Button>
+            </li>
+            <li className="extension-splash__browser-list-item">
+              <Button
+                className="extension-splash__install-button"
+                primary={true}
+                disabled={true}
+              >
+                Install for Firefox
+              </Button>
+              <br />
+              <div className="extension-splash__comming-soon">
+                <LabelStyle>(comming soon)</LabelStyle>
+              </div>
+            </li>
+          </ul>
 
-      <p>
-        The Rapid PREreview app let you read and add reviews (or requests for
-        feedback) directly from the preprint sites you visit without having to
-        navigate to the Rapid PREreview homepage.
-      </p>
-
-      <ul>
-        <li>Install for Chrome</li>
-        <li>Install for Firefox (comming soon)</li>
-      </ul>
-
-      <p>
-        Don’t see support for your browser? <a href="#">let us know!</a>
-      </p>
+          <p className="extension-splash__request-browser">
+            Don’t see support for your browser? <br />{' '}
+            <a href="#">let us know!</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
