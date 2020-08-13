649f7257e6f9923d5267e016d162a8c197d369b0
leonardosfl <leonardostfl@gmail.com>
2020-08-12 22:02:24 -0300

    Update banner message

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,8 +7,8 @@ export default function Banner() {
   return (
   <React.Fragment>
     <div className='announcement'>
-      <p>Click <a className='announcement-link' href={`${process.env.API_URL}/dashboard`}>here</a> to find preprints on COVID-19 with reviews or requests for reviews.</p>
-      <p>Click <a className='announcement-link' href="https://oaspa.org/covid-19-publishers-open-letter-of-intent-rapid-review/?highlight=covid-19" target="_blank">here</a> to read OASPA's open letter of intent to ensure rapid review of key work related to COVID-19.</p>
+      <p>Get involved with rapidly reviewing COVID-19 preprints and then view our <a className='announcement-link' href={`${process.env.API_URL}/dashboard`}>COVID-19 Dashboard</a> of review activities and recommendations.</p>
+      <p>Read about the <a className='announcement-link' href="https://oaspa.org/covid-19-publishers-open-letter-of-intent-rapid-review/?highlight=covid-19" target="_blank">OASPA’s initiative</a> to ensure rapid review of key COVID-19 work.</p>
     </div>
   </React.Fragment>
   )
